ec126cd1b3dfa03f8b7906520f707e88bb3614e0
glendell03 <dellbringino@gmail.com>
2021-04-14 00:10:42 +0800

    Setup Tailwind Css

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,8 +4,11 @@ import "./style.css";
 
 const Button = ({ handleClick }) => {
   return (
-    <div>
-      <button className="btn" onClick={handleClick}>
+    <div className="">
+      <button
+        className="p-14 border-2 border-black-500 bg-red-500"
+        onClick={handleClick}
+      >
         Click me
       </button>
     </div>
